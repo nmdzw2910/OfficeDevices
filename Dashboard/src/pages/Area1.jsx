5d90868d50d257ae5123d379708ead55e7853cbb
nmdzw2910 <nmdzw2910@gmail.com>
2021-08-05 19:03:23 +0700

    Summary Chart for each area

--- Dashboard/src/pages/Area1.jsx
+++ Dashboard/src/pages/Area1.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Area1 = () => {
     return (
         <div>
-            <h2 className="page-header">
-                customers
-            </h2>
-            
+            <iframe width="450" height="260" style={{border: "1px solid #cccccc", padding:"10px"}} src="https://thingspeak.com/channels/1449821/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=line"></iframe>
+            <iframe width="450" height="260" style={{border: "1px solid #cccccc", padding:"10px"}} src="https://thingspeak.com/channels/1449821/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Humidity&type=line"></iframe>
+            <iframe width="450" height="260" style={{border: "1px solid #cccccc", padding:"10px"}} src="https://thingspeak.com/channels/1449821/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Air+Quality+Index&type=line"></iframe>
+            <iframe width="450" height="260" style={{border: "1px solid #cccccc", padding:"10px"}} src="https://thingspeak.com/channels/1449821/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Noise+Level&type=line"></iframe>
         </div>
     )
 }
